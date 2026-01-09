dc8795fa709527231692ca830573208ea89ef3e2
Matthew Thomson <matthew.thomson@ravebuild.co.nz>
2026-01-10 10:53:42 +1300

    fix mdx not building, analytics script blocked

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             </header>
             <main>{children}</main>
           </div>
-          <Analytics />
+          {/*<Analytics />*/}
         </ThemeProvider>
       </body>
     </html>
